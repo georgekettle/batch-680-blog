3aa02df99038ab6b91e96e0784eb9cf01c397b44
George Kettle <georgekettle.gk@gmail.com>
2021-08-27 10:14:49 +1000

    Added cloudinary and image upload to articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+file = URI.open('https://images.unsplash.com/photo-1619347740259-834d6c7ec383?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60')
+article = Article.new(title: 'Flowers', body: "We love flowers")
+article.photo.attach(io: file, filename: 'flowers.jpg', content_type: 'image/jpg')
+article.save
